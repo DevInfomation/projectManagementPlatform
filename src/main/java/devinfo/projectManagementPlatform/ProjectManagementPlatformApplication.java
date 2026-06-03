2c2aefa596ca957e12f01a41b154f68fd38a0b73
devinfo <devinformationproject@gmail.com>
2026-06-03 20:14:29 +0200

    First Commit BBY

---
+++ src/main/java/devinfo/projectManagementPlatform/ProjectManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package devinfo.projectManagementPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devinfo/projectManagementPlatform/ProjectManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package devinfo.projectManagementPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
