fd88d08289ae408415f32044f60dbf8e6a1d4a5d
devinfo <devinformationproject@gmail.com>
2026-06-14 20:56:52 +0200

    Using the Response Entity to give me an ok status if all fields are filled out

--- src/main/java/devinfo/projectManagementPlatform/controllers/UserController.java
+++ src/main/java/devinfo/projectManagementPlatform/controllers/UserController.java
@@ -2,7 +2,10 @@ package devinfo.projectManagementPlatform.controllers;
 
 import devinfo.projectManagementPlatform.models.User;
 import devinfo.projectManagementPlatform.repositories.UserRepository;
+import devinfo.projectManagementPlatform.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,9 +16,12 @@ import java.util.List;
 public class UserController {
     private final UserRepository userRepository;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/createUser", method = RequestMethod.POST)
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
     }
 
     @RequestMapping(path = "/fetchUsers", method = RequestMethod.GET)
