2e6d4184107cffabce0c33d7ee7af121e1e84af3
devinfo <devinformationproject@gmail.com>
2026-06-17 21:34:04 +0200

    refactor: changing from request mapping to get and post mappings

--- src/main/java/devinfo/projectManagementPlatform/controllers/TaskController.java
+++ src/main/java/devinfo/projectManagementPlatform/controllers/TaskController.java
@@ -20,17 +20,17 @@ public class TaskController {
     @Autowired
     TaskService taskService;
 
-    @RequestMapping(path = "/createTask", method = RequestMethod.POST)
+    @PostMapping("/createTask")
     public Task createTask(@RequestBody Task task) {
         return taskService.createTask(task);
     }
 
-    @RequestMapping(path = "/fetchAllTasks", method = RequestMethod.GET)
+    @GetMapping("/fetchAllTasks")
     public List<Task> fetchAllTasks() {
         return taskRepository.findAll();
     }
 
-    @RequestMapping(path = "task/{id}", method = RequestMethod.GET)
+    @GetMapping("/task/{id}")
     public Task getById(@PathVariable Long id) {
         return taskRepository.getById(String.valueOf(id));
     }
